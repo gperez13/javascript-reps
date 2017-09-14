065dd1a21ffe7de46436363f0f33075fb763afac
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-14 11:56:18 -0500

    I attempted #3 and will need to come back to it

--- keepup.js
+++ keepup.js
@@ -20,20 +20,48 @@ const example = (object) => {
 example(['apple','orange','mesopotamia','surethang'])
 
 
-//round 2
+//round 2 ----NEEDS WORK
+
+const transmogrifier = (one, two, three) => {
+	const mult = (one * two);
+	const expo = Math.pow(three); 
+
+	let result = expo.mult;
+	}
+
+ return result
+
+}
 
 
+transmogrifier(4, 5, 3)
+
+//Round 3-  NEEDS WORK
+ 
+const toonify = (accent, sentence) => {
 
 
+	if(accent ='daffy') {
+			for(let i = 0; i <= sentence.length; i ++){
+				if (i === 's') {
+					sentence[i].replace('s', 'th')
+				} 
+		}
+	} else if (accent === 'elmer') {
+			for(let i = 0; i <= sentence.length; i ++) {
+				if (i === 'r') {
+				sentence[i].replace('r', 'w')
+		}
+	} else {
+		console.log(toonify)
+	}
 
 
+	return sentence
+	}
+}
 
-
-
-
-//Round 3
-
-
+tonnify ('daffy', "so you smell like sausage")
 
 
 
