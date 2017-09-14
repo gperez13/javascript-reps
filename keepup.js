d07508a6dc6641599d789ef399ba6906e06df447
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-14 11:06:18 -0500

    KOd Round 1

---
+++ keepup.js
@@ -0,0 +1,40 @@
+
+
+//Round 1
+
+const example = (object) => {
+ 	let numbers = '';
+
+ for(let i = 0; i < object.length; i++) {
+ 	if(object[i].length >= 0){
+
+ 		numbers += object[i].length;
+ 	}
+ }
+ return numbers
+
+}
+
+
+
+example(['apple','orange','mesopotamia','surethang'])
+
+
+//round 2
+
+
+
+
+
+
+
+
+
+
+//Round 3
+
+
+
+
+
+
